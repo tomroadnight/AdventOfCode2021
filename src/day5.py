457ca06c50c90766146761bb92a0e9d3b531d63c
Tom Roadnight <thomasroadnight@gmail.com>
2021-12-05 14:57:55 +0000

    Day 5.

---
+++ src/day5.py
@@ -0,0 +1,39 @@
+import collections
+import typing
+from lib.importer import read_file
+
+# Bresnham Line Algorithm - (https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)
+def bresenham_line_algorithm(x0: int, y0: int, x1: int, y1: int) -> typing.Set[typing.Tuple[int, int]]:
+    line = set()
+    dx =  abs(x1 - x0)
+    dir_x = 1 if x0 < x1 else -1
+    dy = -abs(y1 - y0)
+    dir_y = 1 if y0 < y1  else -1
+    d = dx + dy
+    for z in (range(x0, x1 + dir_x, dir_x) if dx else range(y0, y1 + dir_y, dir_y)):
+        line.add((z, y0) if dx else (x0, z))
+        if dx and 2*d <= dx:
+            d += dy
+            y0 += dir_y
+        if dy and 2*d >= dy:
+            d += dx
+            x0 += dir_x
+    return line
+
+
+def part_a_b() -> typing.Tuple[int, int]:
+    lines = read_file('day5', line_cb=lambda x: list(map(int, x.replace(' -> ', ',').split(','))))  
+    vents = collections.defaultdict(lambda: 0)
+    vents_with_diag = collections.defaultdict(lambda: 0)
+    for line in lines:
+        vent_line = bresenham_line_algorithm(*line)
+        for l in vent_line:
+            vents_with_diag[l] += 1
+        if line[0] == line[2] or line[1] == line[3]:
+            for l in vent_line:
+                vents[l] += 1
+    return len(list(filter(lambda x: x>1, vents.values()))), len(list(filter(lambda x: x>1, vents_with_diag.values())))
+
+
+if __name__ == '__main__':
+    print(part_a_b())
--- src/lib/importer.py
+++ src/lib/importer.py
@@ -1,7 +1,9 @@
 from typing import Callable
 
-def read_file(file_name: str, line_cb: Callable = None, line_filter_cb: Callable = None) -> list:
+def read_file(file_name: str, line_cb: Callable = None, line_filter_cb: Callable = None, post_cb_line_filter: Callable = None) -> list:
     with open(f'src/data/{file_name}.txt') as file:
         lines = file.readlines()
         lines = [line_cb(line.rstrip()) if line_cb else line.rstrip() for line in lines if not line_filter_cb or line_filter_cb(line.rsplit())]
+        if post_cb_line_filter:
+            lines = list(filter(post_cb_line_filter, lines))
     return lines

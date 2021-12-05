727366b8b3e6c3588e3cb647db5c2cc393185c1f
Tom Roadnight <thomasroadnight@gmail.com>
2021-12-05 14:58:04 +0000

    Day 4 update.

--- src/day4.py
+++ src/day4.py
@@ -11,7 +11,7 @@ def part_a_b() -> typing.Tuple[int, int]:
     board_size = 5
     lines = read_file('day4', line_filter_cb=bool)
     balls = list(map(int, lines[0].split(',')))
-    boards = [[list(map(int, y.split())) for y in lines[(1+x):(1+board_size+x)]] for x in range(0, len(lines)-1, 5)]
+    boards = [[list(map(int, y.split())) for y in lines[(1+x):(1+board_size+x)]] for x in range(0, len(lines)-1, board_size)]
 
     board_completion_summary = []
 
@@ -20,7 +20,7 @@ def part_a_b() -> typing.Tuple[int, int]:
         for board_idx, board in enumerate(boards):
             if idx := board_num_index(ball, board):
                 board[idx[0]][idx[1]] = -1
-                if (sum(board[idx[0]]) == -5) or (sum(x[idx[1]] for x in board) == -5):
+                if (sum(board[idx[0]]) == -board_size) or (sum(x[idx[1]] for x in board) == -board_size):
                     board_completion_summary.append(sum(max(y, 0) for x in board for y in x) * ball)
                     boards_to_pop.append(board_idx)
         if not (len(boards) - len(boards_to_pop)):
